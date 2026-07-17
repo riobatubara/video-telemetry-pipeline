bbcda5378746aed4761fa614a7c96d5caf07a630
riobatubara <outrageryo@gmail.com>
2026-07-17 17:15:22 +0700

    chore: change datatype column play to array(int64)

--- database/clickhouse/02_video_measure.sql
+++ database/clickhouse/02_video_measure.sql
@@ -13,14 +13,14 @@ CREATE TABLE video_measure (
   
   -- THE REPEATING/DUPLICATE EVENTS (Stored as lists to capture everything)
   bitrate                  Array(String),   -- Stores: ['6000,128', '2400,128']
-  buffer                   Array(Int64),    -- Stores: [1783682317985, 1783682318286]
-  playing                  Array(String),   -- Stores: ['1783682318587,5.00', '1783682319389,10.00']
+  play                     Array(Int64),    -- Stores: [1784247707871, 1784247707871]
+  buffer                   Array(Int64),    -- Stores: [1784247707872, 1784247709548]
+  playing                  Array(String),   -- Stores: ['1784247706061,138.00', '1784247706866,143.00']
   seek                     Array(String),   -- Stores: ['1783682318000,25.00']
-  pause                    Array(Int64),    -- Stores: ['1750482986082']
+  pause                    Array(Int64),    -- Stores: ['1784247706868']
 
   -- SINGLE-OCCURRENCE LIFECYCLE FIELDS
   load                       Nullable(Int64),
-  play                       Nullable(Int64),
   complete                   Nullable(Int64),
   unload                     Nullable(Int64),
   error                      Nullable(String),
